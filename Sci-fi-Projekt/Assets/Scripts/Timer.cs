2c62423d0e12781377b66fe64302a5e96e5f48ea
kaiorussell <kaiorussell@gmail.com>
2026-04-25 21:04:52 +0200

    Countdowntimer added

---
+++ Sci-fi-Projekt/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    
+    private TextMeshProUGUI textComponent;
+
+    [SerializeField] private float maxTime = 120;
+    private float timeLeft;
+    private int minutesLeft;
+    private int secondsLeft;
+
+    private void Awake()
+    {
+        textComponent = GetComponent<TextMeshProUGUI>();
+    }
+    void Start()
+    {
+        timeLeft = maxTime;
+    }
+
+    
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        minutesLeft = Mathf.FloorToInt(timeLeft/60f);
+        secondsLeft = Mathf.FloorToInt(timeLeft - minutesLeft * 60f);
+        textComponent.text = $"{minutesLeft}:{secondsLeft}";
+    }
+}
