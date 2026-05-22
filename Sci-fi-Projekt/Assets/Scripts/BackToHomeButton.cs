ef662d2c6f8c6190e85418e5f0a82dee81cfef29
kaiorussell <kaiorussell@gmail.com>
2026-05-22 11:33:34 +0200

    home button werks

--- Sci-fi-Projekt/Assets/Scripts/BackToHomeButton.cs
+++ Sci-fi-Projekt/Assets/Scripts/BackToHomeButton.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackToHomeButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public void GoToHome()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(2);
     }
 }
